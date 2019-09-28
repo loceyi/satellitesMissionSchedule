33144ee24f69f4b7caacdf0f5409c6002eed7c0e
Loceyi <45290120+loceyi@users.noreply.github.com>
2019-09-28 10:00:25 +0800

    Initial Commit

---
+++ Mission schedule with Gurobi(According to the paper 'Optimal Continous-Time...')/gurobi_test_example.py
@@ -0,0 +1,25 @@
+from gurobipy import *
+try:
+# Create a new model
+    m = Model("mip1")
+# Create variables
+    x = m.addVar(obj = 1,vtype=GRB.BINARY, name="x")
+    y = m.addVar(obj = 1,vtype=GRB.BINARY, name="y")
+    z = m.addVar(obj = 2,vtype=GRB.BINARY, name="z")
+# Integrate new variables
+    m.update()
+# Set objective
+    m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
+# Add constraint: x + 2 y + 3 z <= 4
+    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
+# Add constraint: x + y >= 1
+    m.addConstr(x + y >= 1, "c1")
+    m.optimize()
+    for v in m.getVars():
+        print (v.varName, v.x)
+    print ('Obj:', m.objVal)
+except GurobiError:
+    print ('Error reported')
+
+
+
