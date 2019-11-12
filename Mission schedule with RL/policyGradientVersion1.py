94d731f07993888b04e6b1010a6b6d255e09d849
Loceyi <45290120+loceyi@users.noreply.github.com>
2019-11-12 17:27:39 +0800

    2019.11.12 添加了policy gradient的文件，开始学习具体如何构造一个完整的算法。

---
+++ Mission schedule with RL/policyGradientVersion1.py
@@ -0,0 +1,137 @@
+#First set up the policy network
+
+
+import sys
+import torch
+import gym
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+# Constants
+GAMMA = 0.9
+
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, learning_rate=3e-4):
+        super(PolicyNetwork, self).__init__()
+
+        self.num_actions = num_actions
+
+        '''
+        nn.Linear 线性变换，加权重，加偏置.y=Ax+b
+        '''
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size, num_actions)
+
+
+        '''
+        torch.optim是一个实现了多种优化算法的包
+        为了使用torch.optim，需先构造一个优化器对象Optimizer，用来保存当前的状态
+        ，并能够根据计算得到的梯度来更新参数
+        Adam Adam 是一种可以替代传统随机梯度下降（SGD）过程的一阶优化算法，
+        它能基于训练数据迭代地更新神经网络权重
+        要构建一个优化器optimizer，
+        你必须给它一个可进行迭代优化的包含了所有参数（所有的参数必须是变量s）的列表。
+        然后，您可以指定程序优化特定的选项，例如学习速率，权重衰减等
+        '''
+
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))#Relu: f(x)=max(0,x)
+        x = F.softmax(self.linear2(x), dim=1) #softmax层，将输入转换成概率输出。
+        return x
+
+    def get_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.forward(Variable(state))
+        highest_prob_action = np.random.choice(self.num_actions, p=np.squeeze(probs.detach().numpy()))
+        log_prob = torch.log(probs.squeeze(0)[highest_prob_action])
+        return highest_prob_action, log_prob
+#The update function
+
+
+def update_policy(policy_network, rewards, log_probs):
+    discounted_rewards = []
+
+    for t in range(len(rewards)):
+        Gt = 0
+        pw = 0
+        for r in rewards[t:]:
+            Gt = Gt + GAMMA ** pw * r
+            pw = pw + 1
+        discounted_rewards.append(Gt)
+
+    discounted_rewards = torch.tensor(discounted_rewards)
+    discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (
+                discounted_rewards.std() + 1e-9)  # normalize discounted rewards
+
+    policy_gradient = []
+    for log_prob, Gt in zip(log_probs, discounted_rewards):
+        policy_gradient.append(-log_prob * Gt)
+
+    policy_network.optimizer.zero_grad()
+    policy_gradient = torch.stack(policy_gradient).sum()
+    policy_gradient.backward()
+    policy_network.optimizer.step()
+
+#The main loop
+
+def main():
+    env = gym.make('CartPole-v0')
+    policy_net = PolicyNetwork(env.observation_space.shape[0], env.action_space.n, 128)
+
+    max_episode_num = 5000
+    max_steps = 10000
+    numsteps = []
+    avg_numsteps = []
+    all_rewards = []
+
+    for episode in range(max_episode_num):
+        state = env.reset()
+        log_probs = []
+        rewards = []
+
+        for steps in range(max_steps):
+            env.render()
+            action, log_prob = policy_net.get_action(state)
+            new_state, reward, done, _ = env.step(action)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+
+            if done:
+                update_policy(policy_net, rewards, log_probs)
+                numsteps.append(steps)
+                avg_numsteps.append(np.mean(numsteps[-10:]))
+                all_rewards.append(np.sum(rewards))
+                if episode % 1 == 0:
+                    sys.stdout.write("episode: {}, total reward: {}, average_reward: {}, length: {}\n".format(episode,
+                                                                                                              np.round(
+                                                                                                                  np.sum(
+                                                                                                                      rewards),
+                                                                                                                  decimals=3),
+                                                                                                              np.round(
+                                                                                                                  np.mean(
+                                                                                                                      all_rewards[
+                                                                                                                      -10:]),
+                                                                                                                  decimals=3),
+                                                                                                              steps))
+                break
+
+            state = new_state
+
+    plt.plot(numsteps)
+    plt.plot(avg_numsteps)
+    plt.xlabel('Episode')
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    main()
