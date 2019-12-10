69c2fa104722a859fbcf8ee93d8801a3e18976e2
Loceyi <45290120+loceyi@users.noreply.github.com>
2019-12-10 15:21:28 +0800

    2019.12.10 画出了Timewindow的图，肉眼初步验证了一下A3C的结果

--- Mission schedule with RL/twCsvFileRead.py
+++ Mission schedule with RL/twCsvFileRead.py
@@ -1,6 +1,6 @@
 import csv
 import pandas as pd
-
+import matplotlib.pyplot as plt
 '''指定读取某一行'''
 # with open('ThirtyTwoTargets.csv','r') as csvfile:
 #     reader = csv.reader(csvfile)
@@ -17,14 +17,29 @@ Access_End = pd.read_csv('ThirtyTwoTargets.csv',
                 usecols=['Access End (UTCG)'])
 Access_End_list=Access_End.iloc[:,0].tolist()
 
+for i in range(len(Access_Start_list)):
+
+    x = [[Access_Start_list[i], Access_End_list[i]]] # 要连接的两个点的坐标
+    y = [[i/10, i/10]]
+    plt.plot(x[0], y[0], color='r')
+    plt.scatter(x[0], y[0], s=5,color='b')
+    plt.text(x[0][0], y[0][0], '% i' % (i+1), fontsize=10)
+    # ax.text(2, 6, r'an equation: $E=mc^2$', fontsize=15)
+
+plt.show()
+
+
 Task_dict = {}
+
+
+
 
 for i in range(len(Access_Start_list)):
 
     Task_dict.update({'%i' % i: [Access_Start_list[i],Access_End_list[i]
                                  ,1,2]})
 
-print(Task_dict)
+# print(Task_dict)
 # a=Access_Start.iloc[1,0]
 # b=Access_End.iloc[1,0]
 
